cb747210a7ff9039282ed65f34ebdc51fbedfbcf
Maximilian Kürschner <dev@maximilian-kuerschner.de>
2022-04-30 15:24:35 +0200

    add: initalize canvas

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+const height = 1080;
+const width = 1920;
+
+// resize canvas (CSS does scale it up or down)
+canvas.height = height;
+canvas.width = width;
