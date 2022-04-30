06ce9795d914953711c276cfcc76d3ab51d147a4
Maximilian Kürschner <dev@maximilian-kuerschner.de>
2022-04-30 15:25:29 +0200

    add: create path

--- js/main.js
+++ js/main.js
@@ -9,3 +9,10 @@ canvas.height = height;
 canvas.width = width;
 
 context.fillRect(100, 150, 200, 300);
+// create a stroke
+context.beginPath();
+context.moveTo( 50, 100);
+context.lineTo(100, 150);
+context.stroke();
+// this ends the current path
+context.beginPath();
